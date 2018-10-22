a02cd6a8c22dbfa401bf3a7012ca58fbc9e34694
boblemarin <emeric@minimal.be>
2018-10-22 11:56:02 +0200

    more navigation

--- spiral.js
+++ spiral.js
@@ -17,10 +17,11 @@ var SpiralMenu = function( canvas, content ) {
   this.canvas = canvas;
   this.content = content;
   this.container = canvas.parentElement;
+  this.labelContainer = document.getElementById("spiral-label-container");
 
   // visual elements
   var self = this;
-  var configuration = new SpiralConfig( canvas, 50 );
+  var configuration = new SpiralConfig( canvas, 80 );
   var numSpirals = content.length;
   var spirals = [];
     
@@ -37,58 +38,159 @@ var SpiralMenu = function( canvas, content ) {
   var labelPool = [];
   var labels = [];
 
+
+
+  // prepare startup content
   for ( var i = 0; i < numSpirals; ++i ) {
+    prepareContent( content[i], 0 );
     spirals.push( new Spiral( i * Math.PI, Math.PI * 0.8 ) );
     var sl = [];
     var nn = content[i].content.length;
     for ( var j = 0; j < nn; ++j ) {
-      var label = new SpiralLabel( content[i].content[j], this.container, configuration );
+      var label = new SpiralLabel( content[i].content[j], this.labelContainer, configuration );
       sl.push( label );
     }
     labels.push( sl );
   }
 
   window.addEventListener( 'resize',    onSpiralResize );
-  this.canvas.addEventListener( 'mousedown', onSpiralDragMouseDown );
-  this.canvas.addEventListener( 'mouseup',   onSpiralDragMouseUp );
+  this.container.addEventListener( 'mouseup',   onContainerMouseUp );
   this.container.addEventListener( 'mousedown', onContainerMouseDown );
 
   render();
 
+  function prepareContent( node, iterator ) {
+    node.open = false;
+    node.level = iterator;
+    if ( node.hasOwnProperty( 'content' ) ) {
+      for ( let child of node.content ) {
+        prepareContent( child, iterator + 1 );
+      }
+    }
+  }
+
+  function updateMenu() {
+    // clean current menu
+    labels = [];
+    self.labelContainer.innerHTML = "";
+
+    // parse content and create labels
+    for ( var i = 0; i < numSpirals; ++i ) {
+      var sl = [];
+      for ( let node of content[i].content ) {
+        addToMenu( node, sl );
+      }
+      labels.push( sl );
+    }
+  }
+
+  function addToMenu( node, array ) {
+    // create label for node
+    var label = new SpiralLabel( node, self.labelContainer, configuration );
+    array.push(label);
+    // resurse for opened sections
+    if ( node.type == "section" && node.open == true) {
+      for ( let child of node.content ) {
+        addToMenu( child, array );
+      }
+    }
+  }
+
   function onSpiralResize( event ) {
     configuration.center();
-  }
-
-  function onContainerMouseDown( event )  {
-    if ( event.target.classList.contains( 'node' ) ) {
-      console.log(event.target.innerHTML);
-    }
-    event.preventDefault();
-    event.stopImmediatePropagation();
-  }
-
-  function onSpiralDragMouseDown( event ) {
-    isMouseDown = true; 
-    self.canvas.style.cursor = 'ew-resize';
-    gravity = 0.9;
-    pMouseX = event.pageX; 
-    mouseX = pMouseX;
-    self.canvas.addEventListener( 'mousemove', onSpiralDragMouseMove );
     if ( frameReq == 0 ) {
       frameReq = requestAnimationFrame( render );
     }
-    event.preventDefault();
-    // event.stopImmediatePropagation();
   }
 
-  function onSpiralDragMouseMove( event ) {
+  function onContainerMouseDown( event )  {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+
+    if ( event.target.classList.contains( 'node' ) ) {
+      let node = event.target.__node;
+      switch ( node.type ) {
+        case "section":
+          if ( node.open ) {
+            node.open = false;
+          } else {
+            openBranchForNode( node );
+          }
+          updateMenu();
+          spiralSpeed = -0.01;
+          if ( frameReq == 0 ) {
+            frameReq = requestAnimationFrame( render );
+          }
+          break;
+        case "page":
+          console.log("Sould open node : " + node.title_fr);
+          break;
+      }
+      // if ( node.type == "section" ) {
+      //   node.open = !node.open;
+        
+      // }
+      return;
+    }
+
+    isMouseDown = true; 
+    gravity = 0.9;
+    pMouseX = event.pageX; 
+    mouseX = pMouseX;
+    self.canvas.style.cursor = 'ew-resize';
+    self.container.addEventListener( 'mousemove', onContainerMouseMove );
+    if ( frameReq == 0 ) {
+      frameReq = requestAnimationFrame( render );
+    }
+  }
+
+  function openBranchForNode( target ) {
+    for ( let spiral of content) {
+      //console.log("check : " + spiral.title_fr);
+      for ( let node of spiral.content ) {
+        //console.log("check : " + node.title_fr);
+        if ( node.type == 'section' ) {
+          node.open = checkSectionForNode( node, target );
+          //console.log("NODE open("+node.open+") : "+ node.title);
+        }
+      }
+    }
+  }
+
+  function checkSectionForNode( node, target ) {
+    // node is target, should not be opened
+    if ( node == target ) return true;
+
+    let open = false;
+    for ( let child in node.content ) {
+      if ( child == target ) {
+        open = true;
+      }
+    }
+    return open;
+  }
+
+
+    // for ( var i = 0; i < numSpirals; ++i ) {
+    //   var sl = [];
+    //   for ( let node of content[i].content ) {
+    //     addToMenu( node, sl );
+    //   }
+    //   labels.push( sl );
+    // }
+  //}
+
+  function onContainerMouseMove( event ) {
     mouseX = event.pageX;
   }
 
-  function onSpiralDragMouseUp( event ) {
-    isMouseDown = false; 
-    self.canvas.style.cursor = 'pointer';
-    self.canvas.removeEventListener( 'mousemove', onSpiralDragMouseMove );
+  function onContainerMouseUp( event ) {
+    if (isMouseDown) {
+      isMouseDown = false; 
+      self.canvas.style.cursor = 'pointer';
+      self.container.removeEventListener( 'mousemove', onContainerMouseMove );
+    }
+    
   }
 
   function render() {
@@ -113,9 +215,13 @@ var SpiralMenu = function( canvas, content ) {
       var nl = labels[i].length;
       var points = spirals[i].rotate(spiralRotation, configuration, nl );
 
+      configuration.context.beginPath();
+      configuration.context.lineWidth = 2;
+      configuration.context.strokeStyle = 'rgba(255, 255, 255, 0.2)';
       for ( var j = 0; j < nl; ++j ) {
         labels[i][j].follow(points[j]);
       }
+      configuration.context.stroke();
 
       // position labels
     }
@@ -150,7 +256,7 @@ var SpiralConfig = function( canvas, numPoints ) {
   this.nPoints = numPoints;
 
   // Label properties
-  this.inertia = 0.4;
+  this.inertia = 0.5;
   this.k = 0.2;
   this.linkMargin = 5;
   this.scaleLimit = 0.5;
@@ -234,16 +340,21 @@ var SpiralLabel = function( node, container, config ) {
   this.speedX = 0;
   this.speedY = 0;
   this.offsetX = 0;
-  this.offsetY = 0;
+  // this.offsetY = 0;
   this.marginX = 25;
-  this.marginY = 0;
+  // this.marginY = 0;
   this.isLeft = true;
 
   var e = document.createElement( 'span' );
   e.className = 'spiral-flying-label';
-  e.innerHTML = '<span class="node ' + node.type + '">' + node.title_fr + '</span>';
   e.style.left = this.posX + 'px';
   e.style.top = this.posY + 'px';
+
+  var ee = document.createElement( 'span' );
+  ee.className = "node " + node.type + (node.level > 1 ? " sub" : "");
+  ee.innerHTML = node.title_fr;
+  ee.__node = node;
+  e.appendChild(ee);
   container.appendChild(e);
   this.element = e;
 };
@@ -254,18 +365,18 @@ SpiralLabel.prototype.setPosition = function( p ) {
   this.speedX = 0;
   this.speedY = 0;
   this.offsetX = 0;
-  this.offsetY = 0;
+  // this.offsetY = 0;
 };
 
 SpiralLabel.prototype.follow = function( p ) {
   // todo: import logic from FlyingLabel class
 
   if ( p.x < this.config.centerX ) {
-    // this.offsetX = -this.marginX;
+    this.offsetX = -this.marginX * this.node.level;
     this.isLeft = true;
     this.element.classList.add('left');
   } else {
-    // this.offsetX = this.marginX;
+    this.offsetX = this.marginX * this.node.level;
     this.isLeft = false;
     this.element.classList.remove('left');
   }
@@ -275,10 +386,14 @@ SpiralLabel.prototype.follow = function( p ) {
   this.posX += this.speedX;
 
   this.speedY *= this.config.inertia;
-  this.speedY += (p.y + this.offsetY - this.posY) * this.config.k;
+  this.speedY += (p.y - this.posY) * this.config.k;
   this.posY += this.speedY;
 
   this.element.style.left = this.posX + 'px';
   this.element.style.top = this.posY + 'px';
   this.element.style.opacity = p.z * 0.8 + 0.2;
+
+  this.config.context.moveTo(this.posX + 25 * this.node.level * (this.isLeft ? -1 : 1), this.posY);
+  this.config.context.lineTo(p.x, p.y);
+
 };
