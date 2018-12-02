f72b149e01c8464a0ebdd19d441726eb7c0dc5a6
boblemarin <emeric@minimal.be>
2018-12-02 15:57:23 +0100

    adding ID property to content nodes

--- paxton.js
+++ paxton.js
@@ -1,32 +1,38 @@
 var menuContent = [
   {
+    "id":       "2c678652-acde-46d5-8870-96747df66fdb",
     "type":     "section",
     "title_fr": "SENSATION ET SENS",
     "title_en": "SENSATION AND SENSES",
     "content":
     [
       {
+        "id":       "d1e2251c-3026-4f3c-871a-b727af8677d1",
         "type":     "section",
         "title_fr": "la pesanteur",
         "title_en": "gravity",
         "content":
         [
           {
+            "id":       "087fb392-02d6-408d-9386-39c0d1cee25d",
             "type":     "page",
             "title_fr": "le plat et le rond",
             "title_en": "the Flat & the Round",
           },
           {
+            "id":       "167486dd-a7e2-4e97-84d8-381b22e16ad1",
             "type":     "page",
             "title_fr": "chutes",
             "title_en": "falls",
           },
           {
+            "id":       "a4d02565-5511-43b0-b9e0-a59e3ea0980d",
             "type":     "page",
             "title_fr": "la gravité dans l'eau",
             "title_en": "water gravity",
           },
           {
+            "id":       "5a8484b7-1aee-407e-ab78-e732f77ca216",
             "type":     "page",
             "title_fr": "ajuster",
             "title_en": "adjusting",
@@ -34,17 +40,20 @@ var menuContent = [
         ]
       },
       {
+        "id":       "27fe4712-c71e-4e07-8fd6-71b2979b8143",
         "type":     "section",
         "title_fr": "la conscience",
         "title_en": "awareness",
         "content":
         [
           {
+            "id":       "59bee1b8-069a-4807-9b91-f272f6c82e53",
             "type":     "page",
             "title_fr": "une approche de la respiration",
             "title_en": "an approach to breathing",
           },
           {
+            "id":       "9b2f0637-3681-4b7b-8bc7-758d55abdd90",
             "type":     "page",
             "title_fr": "la petite danse",
             "title_en": "the small dance",
@@ -52,17 +61,20 @@ var menuContent = [
         ]
       },
       {
+        "id":       "62e549f7-29c3-4388-9c8d-7ed09d53acc8",
         "type":     "section",
         "title_fr": "pointer",
         "title_en": "pointing",
         "content":
         [
           {
+            "id":       "1eb8399d-c06c-42ec-b5dd-260b000f9f04",
             "type":     "page",
             "title_fr": "pointer, les différentes parties",
             "title_en": "parts of pointing",
           },
           {
+            "id":       "66091aca-1347-440e-a816-5219afc7e173",
             "type":     "page",
             "title_fr": "en relation à l'omoplate",
             "title_en": "relation to scapula",
@@ -70,17 +82,20 @@ var menuContent = [
         ]
       },
       {
+        "id":       "a37752d9-befa-4ca5-8bf0-0db4e4e4ba3e",
         "type":     "section",
         "title_fr": "le bassin",
         "title_en": "pelvis",
         "content":
         [
           {
+            "id":       "b7cd049e-bd96-4e7e-9528-df35ea69c132",
             "type":     "page",
             "title_fr": "le bassin comme initiateur",
             "title_en": "pelvis as an initiator",
           },
           {
+            "id":       "b0c4afc1-7e3f-4a13-bd6f-5ca77dad1a55",
             "type":     "page",
             "title_fr": "exercice: les ischions vers les talons",
             "title_en": "exercice ischia to heels",
@@ -88,27 +103,32 @@ var menuContent = [
         ]
       },
       {
+        "id":       "db29aec0-aa4a-49d5-bf32-213ce61fff58",
         "type":     "section",
         "title_fr": "la marche",
         "title_en": "walk",
         "content":
         [
           {
+            "id":       "aa332729-8674-4d5f-b12a-31d8b3d8933b",
             "type":     "page",
             "title_fr": "la marche comme fondation",
             "title_en": "walk as foundation",
           },
           {
+            "id":       "2d6d5852-3627-418c-9f59-8fd1243765ad",
             "type":     "page",
             "title_fr": "les oiseaux marchent",
             "title_en": "birds walk",
           },
           {
+            "id":       "63a6fb08-4276-4353-92d0-d5c4cef156a7",
             "type":     "page",
             "title_fr": "la marche lente",
             "title_en": "slow walk",
           },
           {
+            "id":       "dbdfd3d5-badb-44ac-9a4d-023f620d44b1",
             "type":     "page",
             "title_fr": "la torsion",
             "title_en": "twist",
@@ -116,17 +136,20 @@ var menuContent = [
         ]
       },
       {
+        "id":       "008941c6-942a-4d6c-a347-a3c1ec7cb3a9",
         "type":     "section",
         "title_fr": "le dos",
         "title_en": "the back",
         "content":
         [
           {
+            "id":       "1ff95e59-376d-4c9d-aa49-4e67af27dab4",
             "type":     "page",
             "title_fr": "se concentrer sur le dos",
             "title_en": "choosing to focus on the Back",
           },
           {
+            "id":       "ed8da83f-2bf2-4aec-93a0-2b4586e91b9e",
             "type":     "page",
             "title_fr": "passage",
             "title_en": "passage",
@@ -134,12 +157,14 @@ var menuContent = [
         ]
       },
       {
+        "id":       "2dfb7564-e97e-4c90-a9f6-c33e67704bd6",
         "type":     "section",
         "title_fr": "l'espace",
         "title_en": "space",
         "content":
         [
           {
+            "id":       "991c3ccd-8ffa-4b20-8d12-06da76f1833a",
             "type":     "page",
             "title_fr": "l'espace sphérique",
             "title_en": "spherical space",
@@ -147,37 +172,44 @@ var menuContent = [
         ]
       },
       {
+        "id":       "04d1730c-14d6-441a-ae79-c56385c440c5",
         "type":     "section",
         "title_fr": "MFS comme base pour l'improvisation",
         "title_en": "MFS as base for improvisation",
         "content":
         [
           {
+            "id":       "87c850a5-fcd9-413c-8f9f-9b4e6643209d",
             "type":     "page",
             "title_fr": "la danse & la culture",
             "title_en": "dance & culture",
           },
           {
+            "id":       "996502d1-c7b9-4c6b-a101-22f5fc32d5e5",
             "type":     "page",
             "title_fr": "introduction aux variations Goldberg",
             "title_en": "looking at Goldberg variations",
           },
           {
+            "id":       "eee910ce-cbe5-4fa5-8fdf-88f83393ab7d",
             "type":     "page",
             "title_fr": "Goldberg variations (87)",
             "title_en": "Goldberg variations (87)",
           },
           {
+            "id":       "cfec027d-3e90-44fe-a4f0-0f586e413423",
             "type":     "page",
             "title_fr": "introduction aux English suites",
             "title_en": "looking at English suites",
           },
           {
+            "id":       "8ac84aa4-1432-4b47-aa4c-e9603ccfaaef",
             "type":     "page",
             "title_fr": "English suites (93)",
             "title_en": "English suites (93)",
           },
           {
+            "id":       "776a5b9a-eb6a-4fcd-b05f-eaebaff93436",
             "type":     "page",
             "title_fr": "une technique pour l'improvisation",
             "title_en": "a technic for improvisation",
@@ -187,43 +219,51 @@ var menuContent = [
     ]
   },
   {
+    "id":       "480ead11-a89b-43cb-9524-95e1e7cb081d",
     "type":     "section",
     "title_fr": "LES FORMES",
     "title_en": "FORMS",
     "content":
     [
       {
+        "id":       "19fb4efa-554a-431a-a3f5-f0c852c072e5",
         "type":     "page",
         "title_fr": "introduction",
         "title_en": "introduction",
       },
       {
+        "id":       "f26fbdbc-2d57-443b-978a-40a2d1c2b536",
         "type":     "section",
         "title_fr": "le rouler croissant",
         "title_en": "crescent roll",
         "content":
         [
           {
+            "id":       "44ff801c-7c00-401a-8b6b-15ab8e2b9db0",
             "type":     "page",
             "title_fr": "initié par les mains et les pieds",
             "title_en": "crescent led by feet and hands",
           },
           {
+            "id":       "41ac7bd4-cbe2-4831-88fc-fad6167d6268",
             "type":     "page",
             "title_fr": "initié par le centre",
             "title_en": "crescent led by center",
           },
           {
+            "id":       "0aa66aca-d77a-4adb-a45a-14b76dc79329",
             "type":     "page",
             "title_fr": "la tête haute",
             "title_en": "head up",
           },
           {
+            "id":       "a17dc51b-2ee3-4932-9bf3-1cd89a9080dd",
             "type":     "page",
             "title_fr": "la tête et les pieds en haut",
             "title_en": "head and feet up",
           },
           {
+            "id":       "1488ddd6-962e-4c97-9041-50b1ef2ea2a4",
             "type":     "page",
             "title_fr": "rouler croissant dans l'eau",
             "title_en": "crescent roll in water",
@@ -231,22 +271,26 @@ var menuContent = [
         ]
       },
       {
+        "id":       "e8c81e80-6ab2-4493-90c1-868e9536a5f3",
         "type":     "section",
         "title_fr": "le rouler hélice",
         "title_en": "helix roll",
         "content":
         [
           {
+            "id":       "1f043667-4acf-426d-8903-5cb957197e81",
             "type":     "page",
             "title_fr": "initié par les pieds",
             "title_en": "led by feet",
           },
           {
+            "id":       "5c91f64f-4460-4555-a199-0c040f907183",
             "type":     "page",
             "title_fr": "initié par les mains",
             "title_en": "led by hands",
           },
           {
+            "id":       "975b25ba-450e-4cd7-9573-2042273bc8e8",
             "type":     "page",
             "title_fr": "dans l'eau",
             "title_en": "in water",
@@ -254,27 +298,32 @@ var menuContent = [
         ]
       },
       {
+        "id":       "2aab818c-a024-42e3-9fde-1b06674119af",
         "type":     "section",
         "title_fr": "les puzzles",
         "title_en": "puzzles",
         "content":
         [
           {
+            "id":       "3860b668-85d7-4289-975d-5b0b3ea91ba5",
             "type":     "page",
             "title_fr": "puzzle 1",
             "title_en": "puzzle 1",
           },
           {
+            "id":       "1cef99c0-9e3f-46ff-aef5-d5df37b5c333",
             "type":     "page",
             "title_fr": "puzzle 2",
             "title_en": "puzzle 2",
           },
           {
+            "id":       "5d6b4ece-c324-4a83-a562-d057788b4255",
             "type":     "page",
             "title_fr": "puzzle 3",
             "title_en": "puzzle 3",
           },
           {
+            "id":       "1b7dde32-cabf-41df-b2ec-252e37c1e4b5",
             "type":     "page",
             "title_fr": "puzzle 4",
             "title_en": "puzzle 4",
@@ -282,32 +331,38 @@ var menuContent = [
         ]
       },
       {
+        "id":       "3c6f3560-f37f-4b80-91c6-2f86dadeea3d",
         "type":     "section",
         "title_fr": "l'ondulation",
         "title_en": "undulation",
         "content":
         [
           {
+            "id":       "bd5ce0ff-c917-4c13-ad2d-f52487e1e6d9",
             "type":     "page",
             "title_fr": "ondulation frontale initiée par la tête",
             "title_en": "frontal undulation initiated by the head",
           },
           {
+            "id":       "0be3452f-6bfe-475e-aae2-4b0bff57389c",
             "type":     "page",
             "title_fr": "ondulation frontale initiée par le bassin",
             "title_en": "frontal undulation initiated by the pelvis",
           },
           {
+            "id":       "365832c2-e02a-4e12-9533-6553a6eeaa3e",
             "type":     "page",
             "title_fr": "ondulation latérale initiée par la tête",
             "title_en": "lateral undulation initiated by the head",
           },
           {
+            "id":       "60c3d505-0b04-47e9-a9e8-1bc1fc5d7d76",
             "type":     "page",
             "title_fr": "ondulation latérale initiée par le bassin",
             "title_en": "lateral undulation initiated by the pelvis",
           },
           {
+            "id":       "132ff967-aca7-429f-ae98-d79388d711ef",
             "type":     "page",
             "title_fr": "le dauphin",
             "title_en": "dolphin",
@@ -315,17 +370,20 @@ var menuContent = [
         ]
       },
       {
+        "id":       "1e9d148d-5d55-4689-bca0-68dd06ac11f4",
         "type":     "section",
         "title_fr": "le rouler d'aikido",
         "title_en": "aikido roll",
         "content":
         [
           {
+            "id":       "72e8815c-5d68-453b-b4df-e4b9df38be46",
             "type":     "page",
             "title_fr": "le principe",
             "title_en": "principle",
           },
           {
+            "id":       "3f40ea0d-55d3-436e-9ab6-907cfa4ac51f",
             "type":     "page",
             "title_fr": "les 3 directions",
             "title_en": "3 directions",
@@ -333,52 +391,62 @@ var menuContent = [
         ]
       },
       {
+        "id":       "f6746f0b-0f92-4466-a5b8-f6af7e6ddee9",
         "type":     "section",
         "title_fr": "regarder les formes",
         "title_en": "looking at the forms",
         "content":
         [
           {
+            "id":       "367d0b68-9508-46cd-9c29-44e7ece7c51b",
             "type":     "page",
             "title_fr": "commentaires sur le rouler croissant initié par les pieds et les mains",
             "title_en": "comments on crescent roll - feet and hands",
           },
           {
+            "id":       "b8f57966-6dff-4117-893e-f4835966ba2e",
             "type":     "page",
             "title_fr": "commentaires sur le rouler croissant initié par le centre",
             "title_en": "comments on crescent roll - center",
           },
           {
+            "id":       "fe034a03-8973-4fa9-866d-1cdde9ec7ee7",
             "type":     "page",
             "title_fr": "commentaires sur le rouler hélice initié par les pieds",
             "title_en": "comments on helix roll - feet",
           },
           {
+            "id":       "b40bd2b9-25ff-40d8-8cc7-d6b22d758a5f",
             "type":     "page",
             "title_fr": "commentaires sur le rouler hélice initié par les mains",
             "title_en": "comments on helix roll - hands",
           },
           {
+            "id":       "18b7826c-dc9d-4355-8f84-9d99f84e0887",
             "type":     "page",
             "title_fr": "la conclusion sur les roulers",
             "title_en": "conclusion on rolls",
           },
           {
+            "id":       "2ee4d5fd-61d3-46de-822a-f9a629f3b750",
             "type":     "page",
             "title_fr": "une démonstration du rouler croissant tête haute",
             "title_en": "crescent roll head up - in workshop",
           },
           {
+            "id":       "8c754574-b9d5-415e-be13-1944c1647695",
             "type":     "page",
             "title_fr": "une démonstration du rouler hélice initié par les pieds",
             "title_en": "helix roll led by feet - in workshop",
           },
           {
+            "id":       "318930ed-3080-42b1-a071-efd03134a63d",
             "type":     "page",
             "title_fr": "une démonstration du rouler hélice initié par les mains",
             "title_en": "helix roll led by hand - in workshop",
           },
           {
+            "id":       "0f12bfce-c0b7-43a0-8420-6ea00b7c665e",
             "type":     "page",
             "title_fr": "puzzle 1 - demo",
             "title_en": "puzzle 1 - demo",
@@ -386,92 +454,110 @@ var menuContent = [
         ]
       },
       {
+        "id":       "17e43dd0-771f-49c2-a9d3-aadcfae2ea71",
         "type":     "section",
         "title_fr": "les échauffements",
         "title_en": "warm ups",
         "content":
         [
           {
+            "id":       "86366616-7fea-4953-a299-8624a29fb4f4",
             "type":     "page",
             "title_fr": "les doigts",
             "title_en": "fingers",
           },
           {
+            "id":       "d7a5eb63-e9b8-4850-b89f-2167c2c956cf",
             "type":     "page",
             "title_fr": "le poignet",
             "title_en": "the wrist",
           },
           {
+            "id":       "c89e5d5a-8b01-4487-8e52-318e60637648",
             "type":     "page",
             "title_fr": "les mains",
             "title_en": "hands",
           },
           {
+            "id":       "49bbe25b-cc7a-41c3-a217-4385800afa71",
             "type":     "page",
             "title_fr": "les bras",
             "title_en": "arms",
           },
           {
+            "id":       "71628794-6c8b-44b8-ba23-c8ce82ccb162",
             "type":     "page",
             "title_fr": "les bras et la colonne vertébrale",
             "title_en": "spine and arms",
           },
           {
+            "id":       "f62b3b0e-0174-4610-8ed0-b1d72aed1231",
             "type":     "page",
             "title_fr": "courber l'os",
             "title_en": "bending bone",
           },
           {
+            "id":       "b913d3d9-8e43-4c5c-893b-4d50eb78a89d",
             "type":     "page",
             "title_fr": "aikido step",
             "title_en": "aikido step",
           },
           {
+            "id":       "53a4983c-dc4b-45dd-b9b0-a00ba23bc91d",
             "type":     "page",
             "title_fr": "la torsion",
             "title_en": "the twist",
           },
           {
+            "id":       "cbb13a42-5088-4da9-bffb-b4b18906a915",
             "type":     "page",
             "title_fr": "torsion des jambes",
             "title_en": "legs twist",
           },
           {
+            "id":       "3a112d46-6f27-4c8c-b204-0e8a44b8bef5",
             "type":     "page",
             "title_fr": "resistance des jambes",
             "title_en": "legs resistance",
           },
           {
+            "id":       "43fb2cda-ec52-4d32-b658-0ddd3aac0c28",
             "type":     "page",
             "title_fr": "trocantere",
             "title_en": "trocantere",
           },
           {
+            "id":       "06662e5b-46a0-4c60-af26-03a9277f9966",
             "type":     "page",
             "title_fr": "psoas",
             "title_en": "psoas",
           },
           {
+            "id":       "d13171bf-bbf9-435c-803c-701efd5029bc",
             "type":     "page",
             "title_fr": "ondulations",
             "title_en": "wave",
           },
           {
+            "id":       "79b96e15-0d83-4fbd-b537-7ae5bd414b35",
             "type":     "page",
             "title_fr": "ondulations au sol",
             "title_en": "wave on the floor",
           },
           {
+            "id":       "34f1e330-eec9-4220-add1-ab01fb79bc30",
             "type":     "page",
             "title_fr": "asanas",
             "title_en": "asanas",
           },
           {
+            "id":       "7f715efd-ac87-4ba7-958b-5a8dc8f1a85f",
             "type":     "page",
             "title_fr": "la sieste",
             "title_en": "napping",
           },
           {
+            "id":       "49c968e6-ea6e-41ee-adb6-f66f62f17e7f",
             "type":     "page",
             "title_fr": "et joue",
             "title_en": "and play",
@@ -479,12 +565,14 @@ var menuContent = [
         ]
       },
       {
+        "id":       "b542c73a-260b-42d9-9fb6-f9cfb73faac1",
         "type":     "section",
         "title_fr": "cultiver",
         "title_en": "cultivating",
         "content":
         [
           {
+            "id":       "8033443d-8cd1-45df-afda-6ab9e5880705",
             "type":     "page",
             "title_fr": "le petit bois & la faux",
             "title_en": "kindling & scythe",
